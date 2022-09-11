b8457aaa9d489e30bd10a5d7c887a1774edef66c
Daniel <danielabdi12@gmail.com>
2022-09-11 16:53:05 +0200

    Add cards and center containers by default

---
+++ src/components/HomePage/Cards.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="flex justify-center text-white">
+    <h2>Het beste voor u, van boer tot boon</h2>
+  </div>
+  <div class="flex justify-evenly">
+    <div class="card w-96 bg-base-100 shadow-xl">
+      <figure class="px-10 pt-10">
+        <img
+          src="https://placeimg.com/400/225/arch"
+          alt="Shoes"
+          class="rounded-xl"
+        />
+      </figure>
+      <div class="card-body items-center text-center">
+        <h2 class="card-title">Shoes!</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div class="card-actions">
+          <button class="btn btn-primary">Buy Now</button>
+        </div>
+      </div>
+    </div>
+    <div class="card w-96 bg-base-100 shadow-xl">
+      <figure class="px-10 pt-10">
+        <img
+          src="https://placeimg.com/400/225/arch"
+          alt="Shoes"
+          class="rounded-xl"
+        />
+      </figure>
+      <div class="card-body items-center text-center">
+        <h2 class="card-title">Shoes!</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div class="card-actions">
+          <button class="btn btn-primary">Buy Now</button>
+        </div>
+      </div>
+    </div>
+    <div class="card w-96 bg-base-100 shadow-xl">
+      <figure class="px-10 pt-10">
+        <img
+          src="https://placeimg.com/400/225/arch"
+          alt="Shoes"
+          class="rounded-xl"
+        />
+      </figure>
+      <div class="card-body items-center text-center">
+        <h2 class="card-title">Shoes!</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div class="card-actions">
+          <button class="btn btn-primary">Buy Now</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Cards",
+};
+</script>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,14 +1,19 @@
 <template>
-  <Hero />
+  <div class="container">
+    <Hero />
+    <Cards />
+  </div>
 </template>
 
 <script lang="ts">
 import Hero from "../components/HomePage/Hero.vue";
+import Cards from "../components/HomePage/Cards.vue";
 
 export default {
   name: "HomePage",
   components: {
     Hero,
+    Cards,
   },
 };
 </script>
