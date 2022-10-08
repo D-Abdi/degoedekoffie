1bc2dd43dd8f68fa1e338580d1bab0c2a652906b
Daniel <danielabdi12@gmail.com>
2022-10-08 23:11:41 +0200

    Add coffee fotos

--- src/components/ProductsPage/ProductsGrid.vue
+++ src/components/ProductsPage/ProductsGrid.vue
@@ -1,23 +1,41 @@
 <template>
-  <section class="flex flex-wrap justify-between">
-    <div class="card w-96 shadow-xl image-full">
+  <section class="grid grid-cols-2 gap-4">
+    <div class="card w-100 shadow-xl image-full">
       <figure>
-        <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
+        <img
+          src="src\assets\images\ecuador-man.png"
+          alt="Ecuadorian man in a coffee field."
+          class="w-full"
+        />
       </figure>
-      <div class="card-body">
-        <h2 class="card-title">Ecuador</h2>
+      <div class="card-body items-center text-center">
+        <h2 class="card-title">Ecuador</2>
+        <img
+          src="src\assets\images\coffee-bag.jpg"
+          alt="A coffee bag surrounded by coffee beans."
+          class="w-60 h-72 rounded"
+        />
         <p>If a dog chews shoes whose shoes does he choose?</p>
         <div class="card-actions justify-end">
           <button class="btn btn-primary">Buy Now</button>
         </div>
       </div>
     </div>
-    <div class="card w-96 shadow-xl image-full">
+    <div class="card w-100 shadow-xl image-full">
       <figure>
-        <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
+        <img
+          src="src\assets\images\guatamala-woman.png"
+          alt="Guatamalan woman carrying plants in a coffee field."
+          class="w-full"
+        />
       </figure>
-      <div class="card-body">
+      <div class="card-body items-center text-center">
         <h2 class="card-title">Guatamala</h2>
+        <img
+          src="src\assets\images\coffee-bag.jpg"
+          alt="A coffee bag surrounded by coffee beans."
+          class="w-60 h-72"
+        />
         <p>If a dog chews shoes whose shoes does he choose?</p>
         <div class="card-actions justify-end">
           <button class="btn btn-primary">Buy Now</button>
