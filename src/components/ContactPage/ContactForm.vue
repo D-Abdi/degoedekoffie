76a11b359a688558d299656859b16ae24004fc09
D-Abdi <56637726+D-Abdi@users.noreply.github.com>
2022-10-29 15:43:14 +0200

    Update ContactForm.vue

--- src/components/ContactPage/ContactForm.vue
+++ src/components/ContactPage/ContactForm.vue
@@ -1,6 +1,6 @@
 <template>
 <section class="text-gray-600 body-font relative">
-  <div class="container px-5 py-24 mx-auto">
+  <div class="container px-5 mx-auto">
     <div class="flex flex-col text-center w-full mb-12">
       <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 custom-color-secondary">Contact Us</h1>
       <p class="lg:w-2/3 text-gray-400 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify.</p>
